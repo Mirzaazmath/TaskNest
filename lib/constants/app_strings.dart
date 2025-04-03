7881a3b0d3b653620b79257c2c9507cfbac8a28a
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-03 15:37:41 +0530

    added app string constant file

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,12 @@
+class AppStrings{
+  //// OnBoarding Text /////
+  static const  String letStart="Let's Start";
+  static const  String onboardingText1="Welcome! 🎉\n Plan smarter, stay organized.";
+  static const  String onboardingText2="Turn your plans into reality stay focused.";
+  static const  String onboardingText3="Every step forward brings you closer to success";
+  //// OnBoarding Image Path /////
+  static const  String onboardingImagePlan="assets/images/planing.png";
+  static const  String onboardingImageComplete="assets/images/complete.png";
+  static const  String onboardingImageSuccess="assets/images/success.png";
+
+}
