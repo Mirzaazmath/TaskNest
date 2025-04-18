54ddd3745e25300da36533a19c92dbb35a3e066f
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-18 10:50:00 +0530

    Refactor code and remove unRequired imports

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
-class AppColors{
-
-  static Color primaryColor=const  Color(0xff484b6a);
-  static Color primaryColorLight=const  Color(0xffe4e5f1);
-  static Color primaryColorDark=const  Color(0xff9394a5);
-  static Color backgroundColor=const Color(0xfffafafa);
-  static Color lightGreyColor=const  Color(0xffd2d3db);
-
+class AppColors {
+  static Color primaryColor = const Color(0xff484b6a);
+  static Color primaryColorLight = const Color(0xffe4e5f1);
+  static Color primaryColorDark = const Color(0xff9394a5);
+  static Color backgroundColor = const Color(0xfffafafa);
+  static Color lightGreyColor = const Color(0xffd2d3db);
 }
--- lib/model/onboarding_model.dart
+++ lib/model/onboarding_model.dart
@@ -3,25 +3,40 @@ import 'package:task_nest/constants/app_strings.dart';
 
 import '../constants/app_colors.dart';
 
-class OnboardingModel{
+class OnboardingModel {
   final String image;
   final String title;
   final Color backgroundColor;
   final Color currentCircleColor;
   final Color nextCircleColor;
   final Color? textColor;
-  OnboardingModel({
-    required this.backgroundColor,
-    required this.nextCircleColor,
-    required this.currentCircleColor,
-    required this.image,
-    required this.title,
-    this.textColor
-  });
+  OnboardingModel(
+      {required this.backgroundColor,
+      required this.nextCircleColor,
+      required this.currentCircleColor,
+      required this.image,
+      required this.title,
+      this.textColor});
 }
-List<OnboardingModel>onboardingDataList=[
- OnboardingModel(backgroundColor:  AppColors.backgroundColor, nextCircleColor:AppColors.backgroundColor, currentCircleColor: AppColors.primaryColor, image: AppStrings.onboardingImagePlan, title: AppStrings.onboardingText1),
-  OnboardingModel(backgroundColor:  AppColors.primaryColor, nextCircleColor:AppColors.backgroundColor, currentCircleColor: AppColors.backgroundColor, image: AppStrings.onboardingImageComplete, title:AppStrings.onboardingText2,textColor: AppColors.backgroundColor),
-  OnboardingModel(backgroundColor:  AppColors.backgroundColor, nextCircleColor:AppColors.primaryColor, currentCircleColor: AppColors.primaryColor, image: AppStrings.onboardingImageSuccess, title: AppStrings.onboardingText3),
 
+List<OnboardingModel> onboardingDataList = [
+  OnboardingModel(
+      backgroundColor: AppColors.backgroundColor,
+      nextCircleColor: AppColors.backgroundColor,
+      currentCircleColor: AppColors.primaryColor,
+      image: AppStrings.onboardingImagePlan,
+      title: AppStrings.onboardingText1),
+  OnboardingModel(
+      backgroundColor: AppColors.primaryColor,
+      nextCircleColor: AppColors.backgroundColor,
+      currentCircleColor: AppColors.backgroundColor,
+      image: AppStrings.onboardingImageComplete,
+      title: AppStrings.onboardingText2,
+      textColor: AppColors.backgroundColor),
+  OnboardingModel(
+      backgroundColor: AppColors.backgroundColor,
+      nextCircleColor: AppColors.primaryColor,
+      currentCircleColor: AppColors.primaryColor,
+      image: AppStrings.onboardingImageSuccess,
+      title: AppStrings.onboardingText3),
 ];
